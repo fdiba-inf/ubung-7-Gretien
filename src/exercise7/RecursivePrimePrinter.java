1717b4975158e96ed5b063a77d7160a0215f69aa
Gretien <gretien.kolev@fdiba-tu.sofia.bg>
2020-11-14 13:37:33 +0000

    a

--- src/exercise7/RecursivePrimePrinter.java
+++ src/exercise7/RecursivePrimePrinter.java
@@ -1,6 +1,7 @@
 package exercise7;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class RecursivePrimePrinter {
     public static void main(String[] args) {
@@ -13,11 +14,21 @@ public class RecursivePrimePrinter {
     }
 
     public static void printPrimes(int n) {
-        // Implement recursive method
+        if(n == 1){
+          System.out.println("1");
+        }else{
+          if(isPrime(n)) System.out.println(n);
+          printPrimes(n-1);
+        }
+
     }
 
     public static boolean isPrime(int n) {
-        // Implement method
-        return false;
+        boolean result = true;
+        for(int i = 2; i <=Math.sqrt(n); i++){
+          if(n%i==0) result = false;
+
+        }
+        return result;
     }
 }
