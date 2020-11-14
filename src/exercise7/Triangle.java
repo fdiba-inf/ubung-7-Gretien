3293fd0ad5385fc3f81a75668c9481a8a40fd2ae
Gretien <gretien.kolev@fdiba-tu.sofia.bg>
2020-11-14 14:25:14 +0000

    a

--- src/exercise7/Triangle.java
+++ src/exercise7/Triangle.java
@@ -12,7 +12,7 @@ public class Triangle {
         for (int line = 1; line <= n; line++){
             printLine(1, line);
         }
-        for(int line = n-1; line > 0 ; line--){
+        for(int line = n-1;line > 0 ; line--){
              printLine(1, line);        
           }
 
@@ -20,9 +20,9 @@ public class Triangle {
     }
 
     public static void printLine(int start, int end){
-        for(int i = start; i < end; i++){
+        for(int i = start; i <= end; i++){
           if(i == end){
-            System.out.print(i);
+            System.out.println(i);
           }else{
             System.out.print(i+" ");
           }
